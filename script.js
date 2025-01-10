7114581a9d0ab5f24690fab061e38c83c56d3cfa
myuser <myemail>
2025-01-09 21:47:03 -0500

    style
    add styles

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let currentLanguage = 'pt';
 
 function toggleLanguage() {
     const welcomeMessage = document.getElementById("welcome-message");
+    const footer = document.getElementById("footer"); 
     if (currentLanguage === 'pt') {
         currentLanguage = 'en';
         welcomeMessage.innerHTML = `You are welcome here!<br>Front-End Developer | My Projects and Networks`;
+        footer.innerHTML = `Created with ♥ by Tiago PdaS`
     } else {
         currentLanguage = 'pt';
         welcomeMessage.innerHTML = `Seja bem-vindo aqui!<br>Desenvolvedor Front-End | Meus Projetos e Redes`;
+        footer.innerHTML = `Criado com ♥ por Tiago PdaS`
+    
     }
 }
